9431f9dca981d4dd9e734670ba5c38251953d157
threecreepio <me@creep.io>
2020-07-22 16:18:40 +0200

    try to prevent excess score printing

--- title/practise.asm
+++ title/practise.asm
@@ -4,6 +4,8 @@ StatusSizes:
 StatusPowers:
 .byte $0, $1, $2, $2
 
+PendingScoreDrawPosition = $7200
+
 TStartGame:
     ; copy bank switching code into wram so the game can call back
     ; to the practise rom!
@@ -18,6 +20,7 @@ TStartGame:
     clc
     
     lda #%00000000
+    sta PendingScoreDrawPosition
     sta PPU_CTRL_REG1
     sta Mirror_PPU_CTRL_REG1
     lda #%00000000
@@ -63,15 +66,21 @@ TStartGame:
 PractiseNMI:
     lda LevelStarting
     bne @Next
+
+    ; update framerule counter
     lda #$14
     cmp IntervalTimerControl
-    bne @CheckForRestartLevel
+    bne @ClearPractisePrintScore
     clc
     lda #1
     ldx #(MathInGameFrameruleDigitStart - MathDigits)
     jsr B10Add
 
-@CheckForRestartLevel:
+@ClearPractisePrintScore:
+    ; check if the new status line has been printed
+    jsr ClearPractisePrintScore
+
+@CheckInput:
     lda JoypadBitMask
     and #(Select_Button | Start_Button)
     beq @Next
@@ -94,13 +103,11 @@ PractiseNMI:
     jmp HotReset
 @CheckForNext:
 @Next:
-
-
     rts
 
+; print world number
 PractiseWriteBottomStatusLine:
     jsr PractisePrintScore
-PractiseWriteBottomStatusLine2:
     ldx VRAM_Buffer1_Offset
     lda #$20                ;write address for world-area number on screen
     sta VRAM_Buffer1,x
@@ -122,10 +129,17 @@ PractiseWriteBottomStatusLine2:
     clc
     adc #$06
     sta VRAM_Buffer1_Offset
-
+@Done:
     inc ScreenRoutineTask
     rts
 
+
+ClearPractisePrintScore:
+    lda VRAM_Buffer1_Offset
+    bne @SkipClear
+    sta PendingScoreDrawPosition
+@SkipClear:
+    rts
 
 PractisePrintScoreLen = 12
 PractiseEnterStage:
@@ -141,48 +155,63 @@ PractiseEnterStage:
 @Done:
     lda #0
     sta LevelEnding
+    ; CONTINUE TO PRINT SCORE
 
+; print framerules and frame counter
 PractisePrintScore:
-    clc
+    ldy PendingScoreDrawPosition
+    bne @RefreshBuffer
+
+    ; set ppu address
     ldy VRAM_Buffer1_Offset
-    lda #$20                ;write address for world-area number on screen
+    lda #$20
     sta VRAM_Buffer1,y
     lda #$63
     sta VRAM_Buffer1+1,y
-    lda #PractisePrintScoreLen                ;write length for it
-    sta VRAM_Buffer1+2,y
-    lda #$00                ;put null terminator on
-    sta VRAM_Buffer1+3+PractisePrintScoreLen,y
-    lda #$2E
-    sta VRAM_Buffer1+3+8, y
-    lda #$24
-    sta VRAM_Buffer1+3+7, y
-    sta VRAM_Buffer1+3+6, y
-    sta VRAM_Buffer1+3+5, y
 
-    sta VRAM_Buffer1+3+0, y
+    ; write length of practise score
+    lda #PractisePrintScoreLen
+    sta VRAM_Buffer1+2,y
+
+    ; put null terminator on
+    lda #$00
+    sta VRAM_Buffer1+3+PractisePrintScoreLen,y
+
+    iny
+    iny
+    iny
+    sty PendingScoreDrawPosition
+
+    ; append length to buffer offset
+    clc
+    adc #(3+PractisePrintScoreLen)
+    sta VRAM_Buffer1_Offset
+
+    lda #$2E
+    sta VRAM_Buffer1+8, y
+    lda #$24
+    sta VRAM_Buffer1+7, y
+    sta VRAM_Buffer1+6, y
+    sta VRAM_Buffer1+5, y
+    sta VRAM_Buffer1+0, y
+@RefreshBuffer:
     lda MathInGameFrameruleDigitStart+3
-    sta VRAM_Buffer1+3+1, y
+    sta VRAM_Buffer1+1,y
     lda MathInGameFrameruleDigitStart+2
-    sta VRAM_Buffer1+3+2,y
+    sta VRAM_Buffer1+2,y
     lda MathInGameFrameruleDigitStart+1
-    sta VRAM_Buffer1+3+3,y
+    sta VRAM_Buffer1+3,y
     lda MathInGameFrameruleDigitStart+0
-    sta VRAM_Buffer1+3+4,y
+    sta VRAM_Buffer1+4,y
 
     lda FrameCounter
     jsr B10DivBy10
-    sta VRAM_Buffer1+3+11, y
+    sta VRAM_Buffer1+11,y
     txa
     jsr B10DivBy10
-    sta VRAM_Buffer1+3+10, y
+    sta VRAM_Buffer1+10,y
     txa
-    sta VRAM_Buffer1+3+9, y
-
-    tya
-    adc #(3+PractisePrintScoreLen)
-    sta VRAM_Buffer1_Offset
-@PractisePrintScoreRTS:
+    sta VRAM_Buffer1+9,y
     ldx ObjectOffset
     rts
 

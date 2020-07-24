fa33bc9dd3aa38521982fd8ff51d369dcc88973e
threecreepio <me@creep.io>
2020-07-24 23:43:43 +0200

    comment flags

--- main.asm
+++ main.asm
@@ -1,8 +1,12 @@
+MMC1 = %00010000
+BATTERY = %00000010
+VERTICAL_MIRROR = %00000001
+
 .segment "INES"
 	.byte "NES",$1A
 	.byte 16 ; prg
 	.byte 1  ; chr
-	.byte $11
+	.byte MMC1 | BATTERY | VERTICAL_MIRROR
 
 .segment "SMBCHR"
 .incbin "smb.chr"
